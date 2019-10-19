8cae152fc60dd59aef8a387079d512aa8b56adce
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-10-19 12:51:53 -0600

    Added image

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -2,23 +2,29 @@ package gui;
 
 import utilities.IConstants;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
 public class MainWindow extends JFrame implements Observer, IConstants {
     private boolean done;
     private JPanel mainPanel;
-    MainWindow(){
+    private BufferedImage image;
+    
+    public MainWindow(){
         //Frame
         super(WINDOW_NAME);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setPreferredSize(new Dimension(WINDOW_WIDTH,WINDOW_HEIGHT));
 
         //MainPanel
         CreateMainPanel();
         //Button
-
 
         //add
         add(mainPanel);
@@ -28,7 +34,22 @@ public class MainWindow extends JFrame implements Observer, IConstants {
         mainPanel = new JPanel();
         mainPanel.setPreferredSize(new Dimension(WINDOW_WIDTH*MAINPANEL_WIDTHRATIO,WINDOW_HEIGHT*MAINPANEL_HEIGHTRATIO));
         //Aqui carga la foto
+        loadImage();
+        JLabel imageLabel = new JLabel(new ImageIcon(this.image.getScaledInstance(WINDOW_WIDTH, WINDOW_HEIGHT, Image.SCALE_DEFAULT)));
+        mainPanel.add(imageLabel);
+        
+        
+        
     }
+    
+    private void loadImage() {
+    	try {                
+            this.image = ImageIO.read(new File(IMAGE_PATH));
+         } catch (IOException ex) {
+              this.image = null;
+         }
+    }
+    
     @Override
     public void update(Observable observable, Object o) {
 
