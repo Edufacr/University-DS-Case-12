d667858894b8b042aabded38d766955a858133eb
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-10-18 18:11:03 -0600

    Created GraphNode

--- src/model/GraphNode.java
+++ src/model/GraphNode.java
@@ -1,5 +1,49 @@
 package model;
 
-public class GraphNode {
+import java.util.ArrayList;
 
+public class GraphNode<T> {
+	private T contents;
+	private boolean visited;
+	private ArrayList<GraphNode<T>> adjacentNodes;
+	
+	public GraphNode(T pContents) {
+		this.contents = pContents;
+		this.adjacentNodes = new ArrayList<GraphNode<T>>();
+	}
+
+	public T getContents() {
+		return this.contents;
+	}
+
+	public ArrayList<GraphNode<T>> getAdjacentNodes() {
+		return adjacentNodes;
+	}
+	
+	public boolean isVisited() {
+		return this.visited;
+	}
+	
+	public void visit() {
+		this.visited = true;
+	}
+	
+	public void resetVisit() {
+		this.visited = false;
+	}
+	
+	public GraphNode<T> getAdjacent(T pValue) {
+		for (GraphNode<T> node : this.adjacentNodes) {
+			if (node.getContents().equals(pValue)) {
+				return node;
+			}
+		}
+		return null;
+	}
+	
+	public void addEdge(GraphNode<T> pNode) {
+		if (!this.adjacentNodes.contains(pNode)) {
+			this.adjacentNodes.add(pNode);
+		}
+	}
 }
