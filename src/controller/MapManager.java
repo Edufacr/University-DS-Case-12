3ee83f8e5c1584cb8e3d17ccd63bbab85df49cc5
Gabriel Vargas <gvargasr@estudiantec.cr>
2019-10-19 10:41:52 -0600

    Added basic graph functionality

--- src/controller/MapManager.java
+++ src/controller/MapManager.java
@@ -1,9 +1,36 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Observable;
+import java.awt.Graphics;
 import model.*;
 
-public class MapManager<T> extends Observable{
-	private Graph<T> graph;
+public class MapManager extends Observable{
+	private Graph<Graphics> graph;
 	//private GUI gui;
+	
+	public MapManager() {
+		this.graph = new Graph<Graphics>();
+	}
+	
+	public void addPoint(Graphics pFirst) {
+		this.graph.addNode(pFirst);
+		setChanged();
+		notifyObservers();
+	}
+	
+	public void addPoint(Graphics pPrevious, Graphics pNew) {
+		this.graph.addNode(pNew);
+		this.graph.addEdge(pPrevious, pNew);
+		this.graph.addEdge(pNew, pPrevious);
+		setChanged();
+		notifyObservers();
+	}
+	
+	public ArrayList<Graphics> getPath(Graphics pSource, Graphics pDestination){
+		setChanged();
+		notifyObservers();
+		return this.graph.getPath(pSource, pDestination);
+	} 
+	
 }
